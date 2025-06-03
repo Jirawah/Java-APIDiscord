a540a5fd7043498dadf1f6c70a542509287b3494
Antoine POUZERATTE <127743130+Jirawah@users.noreply.github.com>
2025-06-03 18:28:45 +0200

    Code refactoring

--- src/main/java/org/example/MyBot.java
+++ src/main/java/org/example/MyBot.java
@@ -6,7 +6,7 @@ import net.dv8tion.jda.api.managers.AudioManager;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.events.ReadyEvent;
-import net.dv8tion.jda.api.events.voice.VoiceStateUpdateEvent; // Ajoutez cette importation
+import net.dv8tion.jda.api.events.voice.VoiceStateUpdateEvent;
 
 import javax.security.auth.login.LoginException;
 
@@ -18,7 +18,7 @@ public class MyBot {
         // Construction du bot avec les permissions nécessaires
         JDABuilder.createDefault(botToken)
                 .enableIntents(GatewayIntent.GUILD_VOICE_STATES) // Permet d'interagir avec les salons vocaux
-                .addEventListeners(new BotVoiceManager()) // Ajouter un gestionnaire pour la connexion vocale
+                .addEventListeners(new BotVoiceManager()) // Ajout d'un gestionnaire pour la connexion vocale
                 .build();
     }
 }
@@ -33,14 +33,13 @@ class BotVoiceManager extends ListenerAdapter {
         // ID du salon vocal à rejoindre
         String voiceChannelId = "1100158192345952378";
 
-        // Obtenir le salon vocal
         channel = event.getJDA().getVoiceChannelById(voiceChannelId);
 
         if (channel != null) {
             // Obtenir le gestionnaire audio de la guilde
             AudioManager audioManager = channel.getGuild().getAudioManager();
 
-            // Connecter le bot au salon vocal
+            // Connexion du bot au salon vocal
             audioManager.openAudioConnection(channel);
             System.out.println("Bot connecté au salon vocal : " + channel.getName());
         } else {
